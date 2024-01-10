2a73397e2ec3bb3794ddce3ba24be5b6c25818fa
nizar-zouaoui <drekieki244@gmail.com>
2024-01-10 13:01:19 +0100

    package now builds successfully

--- scripts/build_package.js
+++ scripts/build_package.js
@@ -1,5 +1,10 @@
 const { execSync } = require("child_process");
 const pkgName = process.argv[2];
-execSync(`yarn run prebuild ${pkgName}`, {
-  stdio: "inherit",
-});
+console.log(process.argv);
+console.log("first");
+execSync(
+  `yarn run pre-build:package ${pkgName} && yarn build --filter=${pkgName}`,
+  {
+    stdio: "inherit",
+  }
+);
