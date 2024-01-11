4e90d875de303aa6fdb185c4dea86172e16fa69b
nizar-zouaoui <drekieki244@gmail.com>
2024-01-11 17:07:56 +0100

    publishing script test

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,11 +2,24 @@ const { execSync } = require("child_process");
 const { getPackageInfo } = require("./lib/getPackageInfo");
 
 const pkgName = process.argv[2];
+const versionUpdate = process.argv[3];
+if (!["patch", "minor", "major"].includes(versionUpdate)) {
+  console.log("Version bump was not provided");
+  process.exit(1);
+}
 
 const pkgInfo = getPackageInfo(pkgName);
+execSync(
+  `yarn build:package ${pkgName} && cd ${pkgInfo.location} && yarn version --${versionUpdate}`,
+  {
+    stdio: "inherit",
+  }
+);
+const pkgJsonDir = path.join(__dirname, "..", pkgInfo.location, "package.json");
+const packageJson = JSON.parse(fs.readFileSync(pkgJsonDir, "utf8"));
 
 execSync(
-  `yarn build:package ${pkgName} && cd ${pkgInfo.location} && yarn publish-package`,
+  `cd ${pkgInfo.location} && yarn publish --accesss public --new-version ${packageJson.version}`,
   {
     stdio: "inherit",
   }
