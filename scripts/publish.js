22276a973013e19d40fb20291595fd552493cc26
nizar-zouaoui <drekieki244@gmail.com>
2024-01-12 14:10:14 +0100

    added publishing to workflow

--- scripts/publish.js
+++ scripts/publish.js
@@ -5,6 +5,10 @@ const fs = require("fs-extra");
 
 const pkgName = process.argv[2];
 
+execSync(`yarn build:package ${pkgName}`, {
+  stdio: "inherit",
+});
+
 const pkgInfo = getPackageInfo(pkgName);
 const pkgJsonDir = path.join(__dirname, "..", pkgInfo.location, "package.json");
 const packageJson = JSON.parse(fs.readFileSync(pkgJsonDir, "utf8"));
