3f651ab2de691042d29b1a9fa24c7da7d42b284e
nizar-zouaoui <drekieki244@gmail.com>
2024-10-13 11:03:55 +0100

    feat: added helmet to dashboard for seo

--- apps/dashboard/src/pages/AboutUs/index.tsx
+++ apps/dashboard/src/pages/AboutUs/index.tsx
@@ -1,8 +1,15 @@
+import SEOHelmet from "../../components/SEO";
 import React from "react";
 
 const AboutUs = () => {
   return (
-    <h1 className="text-center">Welcome to Simple Deliver's About Us page</h1>
+    <>
+      <SEOHelmet
+        title="About Us"
+        description="About Us page is destined to walk you through what, how and why this app is designed."
+      />
+      <h1 className="text-center">Welcome to Simple Deliver's About Us page</h1>
+    </>
   );
 };
 
--- apps/dashboard/src/pages/Login/index.tsx
+++ apps/dashboard/src/pages/Login/index.tsx
@@ -1,8 +1,17 @@
+import SEOHelmet from "../../components/SEO";
 import Login from "../../components/Login";
 import React from "react";
 
 const LoginPage = () => {
-  return <Login />;
+  return (
+    <>
+      <SEOHelmet
+        title="Login"
+        description="Login page is destined to authenticate the user."
+      />
+      <Login />
+    </>
+  );
 };
 
 export default LoginPage;
