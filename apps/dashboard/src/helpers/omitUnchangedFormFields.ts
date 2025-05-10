b790207dbd638be040fb157a8cb715b7b1eaf7f5
nizar-zouaoui <drekieki244@gmail.com>
2025-05-10 18:16:35 +0200

    feat: added a helper to omit unchanged fields from forms

---
+++ apps/dashboard/src/helpers/omitUnchangedFormFields.ts
@@ -0,0 +1,22 @@
+import {
+  DeepMap,
+  DeepPartial,
+  FieldValues,
+  SubmitHandler,
+} from "react-hook-form";
+
+export const omitUnchangedFormFields = <TFieldValues extends FieldValues>(
+  dirtyFields: Partial<DeepMap<DeepPartial<TFieldValues>, boolean>>,
+  onSubmit: SubmitHandler<TFieldValues>
+): SubmitHandler<TFieldValues> => {
+  return (data: TFieldValues) => {
+    const changedFields = Object.keys(dirtyFields) as Array<keyof TFieldValues>;
+
+    const changedData = changedFields.reduce((acc, field) => {
+      acc[field] = data[field];
+      return acc;
+    }, {} as TFieldValues);
+
+    onSubmit(changedData);
+  };
+};
